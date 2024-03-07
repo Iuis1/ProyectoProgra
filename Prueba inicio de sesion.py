acc39f3558e1172717947bc9dd8224583c1ba235
Deivid <130712005+Dyvyd@users.noreply.github.com>
2024-03-07 17:55:29 -0600

    Agregar Inventario
    Se agrega la gestión de agregar inventario

--- Prueba inicio de sesion.py
+++ Prueba inicio de sesion.py
@@ -1,14 +1,15 @@
 
-print("Este es un sistema de prueba de un inicio de seision utilizando unicamente variables")
-
 opciones = 0
 salir = 0
 nomusu1 = 0
 
+#Base de vehículos
+vehiculos = []
+
 
 while True:
 
-    inises=input("Bienvenido desea, crear un usuario(1) o iniciar sesion(2) o ingresar como invitado (3)")
+    inises=input("Bienvenido desea, crear un usuario(1) / iniciar sesion(2) / ingresar como invitado (3) / visualizar inventario")
     print(" ")
 
 
@@ -36,8 +37,88 @@ while True:
 
                     opciones = int(input("Desea gestionar el inventario (1) o desea salir (2)?"))
                 if opciones == 1:
-                    #inventario pendiente
-                    print("Disculpe, pero el inventario está en mantenimiento...")
+
+                    #Gestión de inventario
+
+                    print("Gestión de inventario")
+                    print("Agregar inventario\n")
+
+                    #Variables para el ingreso como administrador
+                    admin = "admin"
+                    contraadmin = "contra"
+
+                    #Solicitud de credenciales de admimnistrador
+                    print("Ingrese el usuario de administrador: ")
+                    user = input(">>")
+                    print("Ingrese la contraseña de administrador: ")
+                    password = input(">>")
+
+                    #Inicio de sesión como admin
+                    if(user == admin and password == contraadmin):
+
+                        print("Inicio de sesión correcto\n")
+
+                        #Variables
+                        marca = "indefinido"
+                        anio = 0
+                        modelo = "indefinido"
+                        cilindraje = 0
+                        precio_de_alquiler = 0
+                        precio = 0
+                        placa = "indefinido"
+                        inhabilitado = False
+                        cantidad_de_vehículos = 0
+
+                        #Variable para determinar cuando termina el ciclo
+                        i = "si"
+
+                        #Ciclo de solicitud de datos del vehículo
+                        while i == "si" or i == "SI" or i == "Si":
+                            
+                            #Pide marca y modelo
+                            print("Ingrese la marca del vehículo: ")
+                            marca = input(">>")
+                            print("Ingrese el modelo del vehículo: ")
+                            modelo = input(">>")
+
+                            #Solicitus de cantidad de vehículos de marca y modelo
+                            print("Ingrese la cantidad de vehículos que requiere agregar de ese modelo: ")
+                            cantidad_de_vehículos = input(">>")
+                            cantidad_de_vehículos = int(cantidad_de_vehículos)
+
+                            #Ingresa la cantidad de vehículos
+                            for j in range (cantidad_de_vehículos):
+                                print("Ingrese el año del vehículo", marca, modelo, "número #", j + 1)
+                                anio = input(">>")
+                                print("Ingrese el cilindraje del vehículo", marca, modelo, "número #", j + 1)
+                                cilindraje = input(">>")
+                                print("Ingrese el precio del alquiler por día del vehículo", marca, modelo, "número #", j + 1)
+                                precio_de_alquiler = input(">>")
+                                print("Ingrese el precio del vehículo", marca, modelo, "número #", j + 1)
+                                precio = input(">>")
+                                print("Ingrese la placa del vehículo", marca, modelo, "número #", j + 1)
+                                placa = input(">>")
+
+                                #Guarda en la matriz los datos del vehículo
+                                vehiculos.append([marca,modelo,anio,cilindraje,precio_de_alquiler,precio,placa,inhabilitado])
+
+
+                            #Determina si se continúa en el ciclo
+                            print("¿Desea seguir ingresando vehículos? si/no")
+                            i = input(">>")
+                            if(i == "no" or i == "No" or i == "NO"):
+                                print("Usted regresará al menú\n")
+                            elif(i == "si" or i == "Si" or i == "SI"):
+                                print("Ingrese un nuevo Vehículo")
+                            else:
+                                print("Ingrese un valor correcto")
+                                i = "si"
+
+                        #Fin del ciclo
+
+                        
+                    else:
+                        print("Usuario o conraseña incorrectos")
 
                 elif opciones == 2:
                     salir = str(input("Si desea salir digite ´si´, en el caso contrario digite ´no´"))
